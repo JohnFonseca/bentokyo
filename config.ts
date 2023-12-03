a4968135f30ad479c4ef7629e66e5f1045cb9da0
João Pedro <81266870+JohnFonseca@users.noreply.github.com>
2023-12-03 11:47:32 -0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -89,7 +89,7 @@ export const config: Config = {
   // Disabling the weather component may cause issues with location-based light/dark switching.
   componentsEnabled: {
     searchBar: false,
-    themeButton: false,
+    themeButton: true,
     clock: false,
     greeter: false,
     date: false,
